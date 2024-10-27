821d26d5480551114bda3fe278206d981aa9388c
Fauzan <hi@fauzanelka.id>
2024-10-27 13:25:53 +0700

    feat: add twig engine

--- api/index.php
+++ api/index.php
@@ -1,15 +1,20 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->addErrorMiddleware(true, false, false);
+
+$app = AppFactory::create();
+
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+
+$app->get('/', '\DataCourse\Controllers\HomeController:home');
+
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->run();
---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DataCourse\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+
+
+class HomeController {
+    private $view;
+
+    public function __construct(Twig $view)
+    {
+        $this->view = $view;
+    }
+    
+    public function home(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    { 
+        return $this->view->render($response, 'index.html.twig');
+    }
+}
